5277da8f10bd64a948324f2bb85ea01227741763
sgsharma <sashasharma@honeycomb.io>
2022-10-12 17:30:41 -0700

    add dataset and dataset dependency

--- board_refinery.tf
+++ board_refinery.tf
@@ -3,37 +3,48 @@ resource "honeycombio_board" "refinery" {
   name  = "${var.refinery_cluster_name} Refinery Operations"
   style = "visual"
   query {
+    dataset             = var.refinery_metrics_dataset
     query_id            = honeycombio_query.refinery-health-query.id
     query_annotation_id = honeycombio_query_annotation.refinery-health-query.id
   }
 
   query {
+    dataset             = var.refinery_metrics_dataset
     query_id            = honeycombio_query.refinery-intercommunication-query.id
     query_annotation_id = honeycombio_query_annotation.refinery-intercommunication-query.id
   }
 
   query {
+    dataset             = var.refinery_metrics_dataset
     query_id            = honeycombio_query.refinery-receive-buffers-query.id
     query_annotation_id = honeycombio_query_annotation.refinery-receive-buffers-query.id
   }
 
   query {
+    dataset             = var.refinery_metrics_dataset
     query_id            = honeycombio_query.refinery-send-buffers-query.id
     query_annotation_id = honeycombio_query_annotation.refinery-send-buffers-query.id
   }
 
   query {
+    dataset             = var.refinery_metrics_dataset
     query_id            = honeycombio_query.refinery-dynsampler-rates-query.id
     query_annotation_id = honeycombio_query_annotation.refinery-dynsampler-rates-query.id
   }
 
   query {
+    dataset             = var.refinery_metrics_dataset
     query_id            = honeycombio_query.refinery-trace-indicators-query.id
     query_annotation_id = honeycombio_query_annotation.refinery-trace-indicators-query.id
   }
 
   query {
+    dataset             = var.refinery_metrics_dataset
     query_id            = honeycombio_query.refinery-sampling-decision-query.id
     query_annotation_id = honeycombio_query_annotation.refinery-sampling-decision-query.id
   }
+
+  depends_on = [
+    honeycombio_dataset.refinery_metrics
+  ]
 }
---
+++ dataset.tf
@@ -0,0 +1,5 @@
+//if this dataset already exists, creating this resource is a no-op.
+resource "honeycombio_dataset" "refinery_metrics" {
+  name = "Refinery Metrics"
+  description = "Refinery metrics"
+}
