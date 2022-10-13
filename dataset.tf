45187f221ea392d002aee5e4fc8cb699dc814f8e
sgsharma <sashasharma@honeycomb.io>
2022-10-12 21:09:08 -0700

    formatting

--- dataset.tf
+++ dataset.tf
@@ -1,5 +1,5 @@
 //if this dataset already exists, creating this resource is a no-op.
 resource "honeycombio_dataset" "refinery_metrics" {
-  name = "Refinery Metrics"
+  name        = "Refinery Metrics"
   description = "Refinery metrics"
 }
